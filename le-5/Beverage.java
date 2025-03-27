36e3ce9e56a9b18cff46a14f2e8db0a50c008115
221B389 <221B389@juet.co.in>
2025-03-27 12:32:16 +0530

    completed

---
+++ le-5/Beverage.java
@@ -0,0 +1,20 @@
+ abstract class Beverage{
+    private void pour(int qty){
+        System.out.println("pour" + qty + "ml of beverage in a glass");
+    }
+    
+    protected abstract void addCondiment();
+    
+    protected  void stir() {}
+    
+    private void serve(){
+        System.out.println("serve through waiter");
+    }
+    
+    public void templateMethod (int qty) {
+        pour(qty);
+        addCondiment();
+        stir();
+        serve();
+    }
+}
---
+++ le-5/Whisky.java
@@ -0,0 +1,9 @@
+class Whisky extends Beverage {
+    protected void addCondiment(){
+        System.out.println("Add some Ice");
+    }
+    
+    protected void stir(){
+        System.out.println("Stir for 30 secs");
+   }
+}
