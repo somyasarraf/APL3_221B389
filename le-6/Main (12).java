11b043248b97f6d04991aefa8af2d2f63c8c4d52
221B389 <221B389@juet.co.in>
2025-04-10 11:16:00 +0530

    completed lab6

---
+++ le-6/Main (12).java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main( String [] args){
+        Offering offering = new Coffee();
+        offering= new Rum(offering);
+        offering = new Rum(offering);
+        System.out.println(offering.getName() + " " + offering.getPrice());
+    }
+}
